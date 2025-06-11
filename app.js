76f6378357e01134faf1062ede7686eebb7410f1
rafiqwe <rmlmanik502@gmail.com>
2025-06-11 20:07:37 +0600

    Add practice folder for practice and write besic code

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const userModel = require("./userModel");
 const app = express();
 const port = 3000;
 
+
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
-  J;
+  res.send("Hello World!");
 });
 
 // How to create data
